c789c47858440ee4e4f46d57772bf4bb6123470b
iapetusbob <haoyuansayhi@gmail.com>
2026-04-06 20:05:28 +0800

    added the 3d files for the LCD case; updated game code; verfied game code works

---
+++ lcd case/README.txt
@@ -0,0 +1,4 @@
+Case for 16x2 LCD by circuito on Thingiverse: https://www.thingiverse.com/thing:2144898
+
+Summary:
+This one-piece case covers Sparkfun's LCD 16x2 display with its built in PCB perfectly. It is perfect for rapid prototyping with this component and a nice addition to your circuito project https://goo.gl/0UNLmG
--- sketches/project_sketch/project_sketch.ino
+++ sketches/project_sketch/project_sketch.ino
@@ -8,7 +8,7 @@ const char* GAME_START1 = "    Welcome!    ";
 const char* GAME_START2 = "ModifiedCangkok!";
 const char* GAME_END1 = "   Game Ended.  ";
 const char* GAME_END2 = "Score: ";
-const char* CONFIRMATION = "  OK:v | NO:x  ";
+const char* CONFIRMATION = " OK: ✓ | NO: X ";
 const char* MARBLES_SCORED = "Points: ";
 const char* GAME_END_CONFIRMATION = "   End Game?    ";
 const char* OBJECTIVE = "Color: ";
@@ -22,16 +22,18 @@ const uint8_t PIN_DOWN = 3;
 const uint8_t PIN_CONFIRM = 4;
 const uint8_t PIN_CANCEL = 5;
 
+const uint8_t POWER_PIN = 50; // Defining Pin 50 as our power source
+
 uint8_t marbles_scored = 0;
 int total_score = 0;
 int colorIdx = 4; // Start on CLEAR
 
 char getButton() {
   while (true) {
-    if (digitalRead(PIN_UP) == LOW) { delay(50); while(digitalRead(PIN_UP) == LOW); return 'U'; }
-    if (digitalRead(PIN_DOWN) == LOW) { delay(50); while(digitalRead(PIN_DOWN) == LOW); return 'D'; }
-    if (digitalRead(PIN_CONFIRM) == LOW) { delay(50); while(digitalRead(PIN_CONFIRM) == LOW); return 'E'; }
-    if (digitalRead(PIN_CANCEL) == LOW) { delay(50); while(digitalRead(PIN_CANCEL) == LOW); return 'C'; }
+    if (digitalRead(PIN_UP) == HIGH) { delay(50); while(digitalRead(PIN_UP) == HIGH); return 'U'; }
+    if (digitalRead(PIN_DOWN) == HIGH) { delay(50); while(digitalRead(PIN_DOWN) == HIGH); return 'D'; }
+    if (digitalRead(PIN_CONFIRM) == HIGH) { delay(50); while(digitalRead(PIN_CONFIRM) == HIGH); return 'E'; }
+    if (digitalRead(PIN_CANCEL) == HIGH) { delay(50); while(digitalRead(PIN_CANCEL) == HIGH); return 'C'; }
   }
 }
 
@@ -45,17 +47,23 @@ void shufflePool() {
 }
 
 void setup() {
-  pinMode(PIN_UP, INPUT_PULLUP);
-  pinMode(PIN_DOWN, INPUT_PULLUP);
-  pinMode(PIN_CONFIRM, INPUT_PULLUP);
-  pinMode(PIN_CANCEL, INPUT_PULLUP);
+  Serial.begin(9600);
+  
+  pinMode(POWER_PIN, OUTPUT);   // Set Pin 50 as an output
+  digitalWrite(POWER_PIN, HIGH); // Send 5V to the pin constanty
+
+  pinMode(PIN_UP, INPUT);
+  pinMode(PIN_DOWN, INPUT);
+  pinMode(PIN_CONFIRM, INPUT);
+  pinMode(PIN_CANCEL, INPUT);
 
   lcd.init();
   lcd.backlight();
   
   lcd.setCursor(0, 0); lcd.print(GAME_START1);
   lcd.setCursor(0, 1); lcd.print(GAME_START2);
-  getButton(); 
+
+  getButton();
 
   randomSeed(analogRead(0)); 
   shufflePool(); 
@@ -64,16 +72,16 @@ void setup() {
 }
 
 void loop() {
+
   lcd.clear();
   lcd.print(OBJECTIVE);
   lcd.setCursor(0, 1);
   lcd.print(COLORS[colorIdx]);
 
   char action = getButton();
-
+  
   if (action == 'E') {
     bool confirmed = false;
-    marbles_scored = 0;
 
     while (!confirmed) {
       lcd.clear();
@@ -83,15 +91,20 @@ void loop() {
       char countAction = getButton();
       if (countAction == 'U' && marbles_scored < 9) marbles_scored++;
       if (countAction == 'D' && marbles_scored > 0) marbles_scored--;
+
+      if (countAction == 'C') {
+        break;              // Exits the "while(!confirmed)" loop
+      }
       
       if (countAction == 'E') {
         lcd.clear();
-        lcd.print(MARBLES_SCORED); lcd.print(marbles_scored);
+        lcd.print(MARBLES_SCORED); lcd.print(marbles_scored*2);
         lcd.setCursor(0, 1); lcd.print(CONFIRMATION);
         
         if (getButton() == 'E') {
-          total_score += marbles_scored;
+          total_score += marbles_scored*2;
           confirmed = true; 
+          marbles_scored = 0;
 
           // Update Cycle Logic
           stepInCycle++;
