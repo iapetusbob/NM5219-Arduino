359ab9b50203fc957469881fd763545fd556f35b
iapetusbob <haoyuansayhi@gmail.com>
2026-04-06 20:05:28 +0800

    added the 3d files for the LCD case; updated game code; verfied game code works

--- sketches/project_sketch/project_sketch.ino
+++ sketches/project_sketch/project_sketch.ino
@@ -1,14 +1,16 @@
+// the repo is available at https://github.com/iapetusbob/NM5219-Arduino
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // Game messages
-const char* GAME_START1 = "    Welcome!    ";
-const char* GAME_START2 = "ModifiedCangkok!";
+const char* GAME_START1 = " Kenangan";
+const char* GAME_START2 = "     Singapura!";
 const char* GAME_END1 = "   Game Ended.  ";
 const char* GAME_END2 = "Score: ";
-const char* CONFIRMATION = "  OK:v | NO:x  ";
+const char* CONFIRMATION = "Confirm | Cancel";
 const char* MARBLES_SCORED = "Points: ";
 const char* GAME_END_CONFIRMATION = "   End Game?    ";
 const char* OBJECTIVE = "Color: ";
@@ -17,22 +19,33 @@ const char* COLORS[] = {"RED", "BLUE", "GREEN", "YELLOW", "CLEAR"};
 int colorPool[] = {0, 1, 2, 3}; 
 int stepInCycle = 0; 
 
-const uint8_t PIN_UP = 2;
-const uint8_t PIN_DOWN = 3;
-const uint8_t PIN_CONFIRM = 4;
-const uint8_t PIN_CANCEL = 5;
+const uint8_t PIN_UP = A0;
+const uint8_t PIN_DOWN = A1;
+const uint8_t PIN_CONFIRM = A2;
+const uint8_t PIN_CANCEL = A3;
+
+const uint8_t POWER_PIN = 50; // Defining Pin 50 as our power source
 
 uint8_t marbles_scored = 0;
 int total_score = 0;
 int colorIdx = 4; // Start on CLEAR
 
+int debounceDelay = 50;
+int lastDebounceTime = 0;
+
 char getButton() {
   while (true) {
-    if (digitalRead(PIN_UP) == LOW) { delay(50); while(digitalRead(PIN_UP) == LOW); return 'U'; }
-    if (digitalRead(PIN_DOWN) == LOW) { delay(50); while(digitalRead(PIN_DOWN) == LOW); return 'D'; }
-    if (digitalRead(PIN_CONFIRM) == LOW) { delay(50); while(digitalRead(PIN_CONFIRM) == LOW); return 'E'; }
-    if (digitalRead(PIN_CANCEL) == LOW) { delay(50); while(digitalRead(PIN_CANCEL) == LOW); return 'C'; }
+    // Serial.print(analogRead(PIN_UP)); Serial.print(" UP ");
+    // Serial.print(analogRead(PIN_DOWN)); Serial.print(" DOWN ");
+    // Serial.print(analogRead(PIN_CONFIRM)); Serial.print(" ENTER ");
+    // Serial.print(analogRead(PIN_CANCEL)); Serial.println(" CANCEL ");
+
+    if (analogRead(PIN_UP) > 900) { delay(50); while(analogRead(PIN_UP) > 900); return 'U'; }
+    if (analogRead(PIN_DOWN) > 900) { delay(50); while(analogRead(PIN_DOWN) > 900); return 'D'; }
+    if (analogRead(PIN_CONFIRM) > 900) { delay(50); while(analogRead(PIN_CONFIRM) > 900); return 'E'; }
+    if (analogRead(PIN_CANCEL) > 900) { delay(50); while(analogRead(PIN_CANCEL) > 900); return 'C'; }
   }
+  
 }
 
 void shufflePool() {
@@ -45,17 +58,23 @@ void shufflePool() {
 }
 
 void setup() {
-  pinMode(PIN_UP, INPUT_PULLUP);
-  pinMode(PIN_DOWN, INPUT_PULLUP);
-  pinMode(PIN_CONFIRM, INPUT_PULLUP);
-  pinMode(PIN_CANCEL, INPUT_PULLUP);
+  Serial.begin(9600);
+  
+  pinMode(POWER_PIN, OUTPUT);   // Set Pin 50 as an output
+  digitalWrite(POWER_PIN, HIGH); // Send 5V to the pin constanty
+
+  pinMode(PIN_UP, INPUT);
+  pinMode(PIN_DOWN, INPUT);
+  pinMode(PIN_CONFIRM, INPUT);
+  pinMode(PIN_CANCEL, INPUT);
 
   lcd.init();
   lcd.backlight();
   
   lcd.setCursor(0, 0); lcd.print(GAME_START1);
   lcd.setCursor(0, 1); lcd.print(GAME_START2);
-  getButton(); 
+
+  getButton();
 
   randomSeed(analogRead(0)); 
   shufflePool(); 
@@ -64,6 +83,7 @@ void setup() {
 }
 
 void loop() {
+
   lcd.clear();
   lcd.print(OBJECTIVE);
   lcd.setCursor(0, 1);
@@ -71,9 +91,10 @@ void loop() {
 
   char action = getButton();
 
-  if (action == 'E') {
+  if (millis() - lastDebounceTime > debounceDelay) {
+
+      if (action == 'E') {
     bool confirmed = false;
-    marbles_scored = 0;
 
     while (!confirmed) {
       lcd.clear();
@@ -81,17 +102,23 @@ void loop() {
       lcd.setCursor(0, 1); lcd.print("Up/Dn or Enter");
 
       char countAction = getButton();
+
       if (countAction == 'U' && marbles_scored < 9) marbles_scored++;
       if (countAction == 'D' && marbles_scored > 0) marbles_scored--;
+
+      if (countAction == 'C') {
+        break;              // Exits the "while(!confirmed)" loop
+      }
       
       if (countAction == 'E') {
         lcd.clear();
-        lcd.print(MARBLES_SCORED); lcd.print(marbles_scored);
+        lcd.print(MARBLES_SCORED); lcd.print(marbles_scored*2);
         lcd.setCursor(0, 1); lcd.print(CONFIRMATION);
         
         if (getButton() == 'E') {
-          total_score += marbles_scored;
+          total_score += marbles_scored*2;
           confirmed = true; 
+          marbles_scored = 0;
 
           // Update Cycle Logic
           stepInCycle++;
@@ -129,4 +156,7 @@ void loop() {
       shufflePool();
     }
   }
+
+  }
+  lastDebounceTime = millis();
 }
